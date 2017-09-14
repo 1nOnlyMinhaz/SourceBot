b059cf21b914c35260fd0fba48dca53580e62762
Alw7SHxD <alw7shxd.ae@gmail.com>
2017-09-14 20:07:39 +0400

    Updated MessageManager

--- src/main/java/bid/ApixTeam/bot/libs/events/MessageReceived.java
+++ src/main/java/bid/ApixTeam/bot/libs/events/MessageReceived.java
@@ -1,7 +1,17 @@
 package bid.ApixTeam.bot.libs.events;
 
+import bid.ApixTeam.bot.utils.BotAPI;
+import bid.ApixTeam.bot.utils.vars.Lists;
+import bid.ApixTeam.bot.utils.vars.RankingType;
+import net.dv8tion.jda.core.entities.ChannelType;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Random;
 
 /**
  * TSC was created by ApixTeam (C) 2017
@@ -9,6 +19,28 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
  */
 public class MessageReceived extends ListenerAdapter {
     public void onMessageReceieved(MessageReceivedEvent e){
+        BotAPI botAPI = new BotAPI();
+        User user = e.getAuthor();
+        Message message = e.getMessage();
 
+        if(e.getTextChannel().getType() == ChannelType.PRIVATE || user.isBot())
+            return;
+
+        if (Lists.getUserRankingCooldown().containsKey(user.getIdLong()) || Lists.getUserRankinCooldown().contains(user.getIdLong()))
+            return;
+
+        botAPI.getDatabaseManager().createUserRanking(user);
+        Random random = new Random();
+        int exp = random.nextInt((25 - 15)) + 15;
+
+        botAPI.getDatabaseManager().giveUserExp(user, exp);
+        botAPI.getMessageManager().logInfo(String.format("[%s] [INFO] @%s#%s earned %d exp.", new Date(),user.getName(), user.getDiscriminator(), exp));
+
+        HashMap<RankingType, Integer> ranking = botAPI.getDatabaseManager().getUserRanking(user);
+        if (Lists.getLevelsMaxExp().get(ranking.get(RankingType.LEVEL)) != null && ranking.get(RankingType.EXPERIENCE) >= Lists.getLevelsMaxExp().get(ranking.get(RankingType.LEVEL)))
+            botAPI.getDatabaseManager().userLevelUp(user);
+
+        Lists.getUserRankinCooldown().add(user.getIdLong());
+        Lists.getUserRankingCooldown().put(user.getIdLong(), 60);
     }
 }
