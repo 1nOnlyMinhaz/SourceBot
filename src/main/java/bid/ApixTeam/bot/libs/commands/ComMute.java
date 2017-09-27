c356f207c6bf1ec87c8f5d2184743085d8d30381
Alw7SHxD <alw7shxd.ae@gmail.com>
2017-09-27 21:02:07 +0400

    Added ComMute

---
+++ src/main/java/bid/ApixTeam/bot/libs/commands/ComMute.java
@@ -0,0 +1,15 @@
+package bid.ApixTeam.bot.libs.commands;
+
+import de.btobastian.sdcf4j.Command;
+import de.btobastian.sdcf4j.CommandExecutor;
+
+/**
+ * TSC-Bot was created by ApixTeam (C) 2017
+ * in association with TheSourceCode (C) 2017
+ */
+public class ComMute implements CommandExecutor {
+    @Command(aliases = {"mute", "shut_up"}, privateMessages = false)
+    public void onCommand(){
+
+    }
+}
