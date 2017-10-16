afbfc72c6c010f76f0912d131b4f66d08a68b8b4
Andrewboy159 <andrewelliott159@gmail.com>
2017-10-16 15:39:44 -0400

    Update Slowmode System

--- src/main/java/bid/ApixTeam/bot/libs/commands/ComSlowmode.java
+++ src/main/java/bid/ApixTeam/bot/libs/commands/ComSlowmode.java
@@ -22,7 +22,7 @@ public class ComSlowmode implements CommandExecutor {
         PermissionManager pm = botAPI.getPermissionManager();
         EmbedMessageManager embedManager = botAPI.getEmbedMessageManager();
 
-        if(!pm.userRoleAtLeast(guild.getMember(user), SimpleRank.SR_MOD)){
+        if(!pm.userRoleAtLeast(guild.getMember(user), SimpleRank.JR_ADMIN)) {
             botAPI.getMessageManager().sendMessage(messageChannel, embedManager.getNoComPermission());
             return;
         }
--- src/main/java/bid/ApixTeam/bot/libs/events/MessageReceived.java
+++ src/main/java/bid/ApixTeam/bot/libs/events/MessageReceived.java
@@ -3,6 +3,7 @@ package bid.ApixTeam.bot.libs.events;
 import bid.ApixTeam.bot.utils.BotAPI;
 import bid.ApixTeam.bot.utils.vars.Lists;
 import bid.ApixTeam.bot.utils.vars.enums.RankingType;
+import bid.ApixTeam.bot.utils.vars.enums.SimpleRank;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
@@ -28,7 +29,7 @@ public class MessageReceived extends ListenerAdapter {
 
         //botAPI.getMessageManager().log(false, String.format("\"%s\"", message.getContent()));
 
-        if (Lists.getSlowmodeChannelCooldown().containsKey(e.getChannel().getIdLong())) {
+        if(Lists.getSlowmodeChannelCooldown().containsKey(e.getChannel().getIdLong()) && !botAPI.getPermissionManager().userAtLeast(user, SimpleRank.JR_ADMIN)) {
             if (Lists.getSlowmodeUserCooldown().containsKey(e.getChannel().getIdLong() + ":" + user.getIdLong())) {
                 long seconds = ((Lists.getSlowmodeUserCooldown().get(e.getChannel().getIdLong() + ":" + user.getIdLong()) / 1000) + Lists.getSlowmodeChannelCooldown().get(e.getChannel().getIdLong())) - (System.currentTimeMillis() / 1000);
                 if (seconds > 0) {
