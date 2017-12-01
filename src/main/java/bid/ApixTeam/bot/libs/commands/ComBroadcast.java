05cc03e53bd72add36c465cb7ce275a55c74d325
Andrewboy159 <andrewelliott159@gmail.com>
2017-11-30 22:43:23 -0500

    Recode ComBroadcast (Not Finished)

--- src/main/java/bid/ApixTeam/bot/libs/commands/ComBroadcast.java
+++ src/main/java/bid/ApixTeam/bot/libs/commands/ComBroadcast.java
@@ -9,24 +9,83 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageChannel;
 import net.dv8tion.jda.core.entities.MessageEmbed;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ComBroadcast implements CommandExecutor {
     @Command(aliases = {"broadcast", "announce", "bc"})
-    public void onCommand(Guild guild, Message command, MessageChannel messageChannel, Object[] objects) {
+    public void onCommand(Guild guild, Message command, MessageChannel messageChannel, String[] args) {
         BotAPI botAPI = new BotAPI();
         EmbedMessageManager embedManager = new EmbedMessageManager();
 
-        if(objects.length != 2) {
-            botAPI.getMessageManager().sendMessage(messageChannel, this.getUsage());
-        } else {
-            String message = (String) objects[0];
-            String channel = (String) objects[1];
+        if(args.length < 3) {
+            botAPI.getMessageManager().sendMessage(messageChannel, getUsage());
+            return;
+        }
 
-            botAPI.getMessageManager().deleteMessage(command, "Auto cleared");
-            botAPI.getMessageManager().sendMessage(guild.getTextChannelById(channel), embedManager.getAsDescription(message));
+        if(args[0].equalsIgnoreCase("now")) {
+            String channel = args[1];
+
+            StringBuilder str = new StringBuilder();
+            for(int i = 2; i < args.length; i++) {
+                str.append(args[i]).append(" ");
+            }
+
+            String message = str.toString().trim();
+
+            Pattern channelPattern = Pattern.compile("\\<\\#(.*?)\\>");
+            Matcher channelMatcher = channelPattern.matcher(channel);
+
+            if(!channelMatcher.find()) {
+                botAPI.getMessageManager().sendMessage(messageChannel, getUsage());
+            } else {
+                String channelId = channelMatcher.group(1);
+
+                botAPI.getMessageManager().sendMessage(guild.getTextChannelById(channelId), embedManager.getAsDescription(message));
+                botAPI.getMessageManager().deleteMessage(command, "Auto Cleared");
+            }
+        } else if(args[0].equalsIgnoreCase("later")) {
+            String timeString = args[1];
+            int time;
+
+            try {
+                time = Integer.parseInt(timeString);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+                botAPI.getMessageManager().sendMessage(messageChannel, getUsage());
+                return;
+            }
+
+            String channel = args[2];
+
+            StringBuilder str = new StringBuilder();
+            for(int i = 3; i < args.length; i++) {
+                str.append(args[i]).append(" ");
+            }
+
+            String message = str.toString().trim();
+
+            Pattern channelPattern = Pattern.compile("\\<\\#(.*?)\\>");
+            Matcher channelMatcher = channelPattern.matcher(channel);
+
+            if(!channelMatcher.find()) {
+                botAPI.getMessageManager().sendMessage(messageChannel, getUsage());
+            } else {
+                String channelId = channelMatcher.group(1);
+
+                botAPI.getMessageManager().deleteMessage(command, "Auto Cleared");
+                botAPI.getMessageManager().sendMessage(guild.getTextChannelById(channelId), embedManager.getAsDescription(message)); //Idk how to queueAfter here, wont let me plx fix father
+            }
+
+        } else if(args[0].equalsIgnoreCase("repeat")) {
+            //Daddy fix plx
+        } else {
+            botAPI.getMessageManager().sendMessage(messageChannel, getUsage());
         }
     }
 
+
     private MessageEmbed getUsage() {
-        return new BotAPI().getEmbedMessageManager().getUsage("!broadcast {message} {channel name}");
+        return new BotAPI().getEmbedMessageManager().getUsage("!broadcast {now|later|repeat} [time (for later & repeat)] {channel} {message}");
     }
 }
